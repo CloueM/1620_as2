d3b3d0e00623f57290a33d09ef5efc9199c81568
Cloue Macadangdang <maczyepez@gmail.com>
2022-04-11 23:13:10 -0700

    Added click titles on navigation bar

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ const notes = [
     id: 1 
   }
 ]
-
 //Add Note Area
 const add_note_btn = document.querySelector('.icons')
 const note_area = document.querySelector('.write-note-area')
@@ -26,7 +25,7 @@ function add_note() {
   note_area.insertAdjacentHTML("afterbegin", add_note_area)
   add_note_btn.removeEventListener('click',add_note)
 
-  //Delete text area
+    //Delete text area
   const delete_btn = document.querySelector('#cancel-button')
   const note_message = document.querySelector('#note-box')
 
@@ -34,6 +33,61 @@ function add_note() {
     console.log(note_message.value);
     note_message.value = '';
   })
+
+  //Save Note
+  const save_btn = document.quarySelector('#save-button');
+  const noteDisplayArea = document.querySelector(".notes-list");
+  const my_id = 1;
+
+  function save_message(){
+    const my_title = note_message.value.split('\n')[0]
+    my_id += 1;
+
+    notes.push({
+      key: my_title,
+      noteBody: note_message.value,
+      id: my_id
+    }) 
+    template = 
+      `  <li>${titles}</li>`;
+    noteDisplayArea.insertAdjacentHTML("beforeend", template);
+  }
+
+  //Click on Side Nav
+  const readNote = document.querySelector(".read-note-area");
+
+  function displayText(event) {
+    if (event.target.localName === "li") {
+      content = event.target.innerText;
+      console.log(content);
+    }
+
+    for (const note of notes) {
+      if (note.title == content) 
+      content = note.noteBody;
+      title2 = note.title;
+    }
+
+    readNoteTemplate = ` 
+      <div id="options">
+          <button class="deleteReadBtn">x</button>
+      <div class="text">
+          <h1>${title2}</h1>
+          <p>${content}</p>
+      </div>  `;
+    readNote.insertAdjacentHTML("afterbegin", readNoteTemplate);
+
+    //Delete the read text
+    const deleteReadBtn = document.querySelector(".deleteReadBtn");
+    function deleteReadText() {
+      while (readNote.hasChildNodes()) {
+        readNote.removeChild(readNote.firstChild);
+      }
+    }
+      deleteReadBtn.addEventListener("click", deleteReadText);
+  }
+  noteDisplayArea.addEventListener("click", displayText);
+  save_btn.addEventListener('click', save_message)
 }
 add_note_btn.addEventListener('click', add_note)
 
